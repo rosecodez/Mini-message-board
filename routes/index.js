d0f45b9abba6f3751dbf780cfe8f2a1a87511d72
Rose <rosebeats09@gmail.com>
2024-04-30 18:17:04 -0400

    Display message board texts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-disable object-shorthand */
+const express = require('express');
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
